bc5411c5ca029e9b33280650ea04d41f07fab707
Shrek75 <freshine0@gmail.com>
2026-01-10 08:48:15 +0900

    Update Pos.cs

--- OOP-Game-Shrek/Objects/BaseObject.cs
+++ OOP-Game-Shrek/Objects/BaseObject.cs
@@ -8,8 +8,29 @@ namespace OOP_Game_Shrek
 {
     internal abstract class BaseObject
     {
+        //기존위치
+        Pos _prevPos;
+
+        //Pos _pos;
+        //public Pos Pos
+        //{
+        //    get { return _prevPos + (_dir * _speed) * 델타타임; }
+        //}
+
+        //방향
+        Pos _dir;
+
+        //속력
+        double _speed;
+
+        //크기
+        double _radius;
+
+
+
         public abstract void Update();
         public abstract void Render();
+
         
     }
 }
--- OOP-Game-Shrek/Utils/Pos.cs
+++ OOP-Game-Shrek/Utils/Pos.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace OOP_Game_Shrek
 {
-    internal class Pos
+    internal struct Pos
     {
+        public double X;
+        public double Y;
+
+        // 크기 1짜리 방향벡터 반환.  인텔리센스 왤캐 좋아;;
+        public static Pos GetDirVector(Pos from, Pos to)
+        {
+            double dx = to.X - from.X;
+            double dy = to.Y - from.Y;
+            double length = Math.Sqrt(dx * dx + dy * dy);
+            if (length == 0)
+            {
+                return new Pos { X = 0, Y = 0 };
+            }
+            return new Pos { X = dx / length, Y = dy / length };
+        }
+
+
+        // 더하기 연산자 오버로딩
+        public static Pos operator +(Pos a, Pos b)
+        {
+            return new Pos { X = a.X + b.X, Y = a.Y + b.Y };
+        }
+
+        // 곱하기 연산자 오버로딩
+        public static Pos operator *(Pos pos, double d)
+        {
+            return new Pos { X = pos.X * d, Y = pos.Y * d };
+        }
+        public static Pos operator *(double d, Pos pos)
+        {
+            return new Pos { X = pos.X * d, Y = pos.Y * d };
+        }
+        public static Pos operator *(Pos pos, int i)
+        {
+            return new Pos { X = pos.X * i, Y = pos.Y * i };
+        }
+        public static Pos operator *(int i, Pos pos)
+        {
+            return new Pos { X = pos.X * i, Y = pos.Y * i };
+        }
     }
 }
